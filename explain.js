a4a89e2cbb535f2a64f58cd53327b8e9c9428642
Stefan Šućur <stefansucur0@gmail.com>
2022-09-29 19:53:33 +0200

    Refactoring
    Move stuff outside of the event listener and make it show the button only on related pages.

--- explain.js
+++ explain.js
@@ -1,42 +1,34 @@
-let btn = document.createElement("div");
+let current = [
+    window.location.protocol,
+    window.location.hostname,
+    window.location.pathname
+]
 
-btn.style.position = "fixed";
-btn.style.right = "20px";
-btn.style.bottom = "20px";
-btn.style.zIndex = "9999999999";
-
-let img = document.createElement("img");
-img.style.position = "relative";
-img.style.height = "48px";
-img.style.cursor = "pointer";
-img.src = browser.runtime.getURL("icons/explain.png");
-
-btn.appendChild(img);
-document.body.appendChild(btn);
-
-btn.addEventListener("click", () => {
-    let current = [
-        window.location.protocol,
-        window.location.hostname,
-        window.location.pathname
-    ]
-    /* window.location.href = concat(
-        [
-            window.location.protocol,
-            window.location.hostname,
-            window.location.pathname,           
-        ]
-    )*/
-
-    if  (isNumeric(current[2]) ||
+if  (isNumeric(current[2]) ||
         !isNumeric(current[2]) && current[2] === "/") {
-            current.splice(1, 0, "//explain")
-            window.open(current.join(""), '_blank').focus()
-        }
-    else {alert("This is not a valid page according to this extension!")}
-    }
-)
+
+    let btn = document.createElement("div");
+
+    btn.style.position = "fixed";
+    btn.style.right = "20px";
+    btn.style.bottom = "20px";
+    btn.style.zIndex = "9999999999";
+
+    let img = document.createElement("img");
+    img.style.position = "relative";
+    img.style.height = "48px";
+    img.style.cursor = "pointer";
+    img.src = browser.runtime.getURL("icons/explain.png");
+
+    btn.appendChild(img);
+    document.body.appendChild(btn);
+
+    btn.addEventListener("click", () => {
+        current.splice(1, 0, "//explain")
+        window.open(current.join(""), '_blank').focus()
+    })
+}
 
 function isNumeric(num){
-  return !isNaN(num.slice(1,-1))
+    return !isNaN(num.slice(1,-1))
 }
